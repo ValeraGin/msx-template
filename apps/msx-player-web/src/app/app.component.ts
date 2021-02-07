3b09094a1c358fb6c590611ed0ccdc2fa6ea6cc4
ValeraGin <valeragin@gmail.com>
2021-02-07 16:18:09 +0300

    initial commit

---
+++ apps/msx-player-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msx-player-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'msx-player-web';
+}
